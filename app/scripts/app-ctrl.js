31ecfedb80c2c3d9ebd5ad3f1a086d2e0da8628a
Samantha Guergenenov <samguergen@gmail.com>
2016-05-22 13:35:53 -0400

    angular app config and controller shell, but angular still not loading

---
+++ app/scripts/app-ctrl.js
@@ -0,0 +1,6 @@
+angular.module('myApp')
+.controller('HomeCtrl', ['$scope', function($scope){
+
+  console.log('inside the controller');
+
+  }]);
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,32 @@
-console.log('angular working');
-console.log(angular);
+angular
+    .module('myApp', [
+        'ngAnimate',
+        'ui.router',
+    ])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider, '$httpProvider') {
+
+    $httpProvider.defaults.withCredentials = true;
+
+      $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'home.html',
+            controller: 'HomeCtrl'
+        });
+        // .state('signup', {
+        //     url: '/signup',
+        //     templateUrl: 'signup.html',
+        //     controller: 'HomeCtrl'
+        // });
+
+    // default fall back route
+    $urlRouterProvider.otherwise('/');
+
+    // enable HTML5 Mode for SEO
+    $locationProvider.html5Mode(true);
+
+    })
+
+.run(function() {
+  console.log('angular app running');
+});
