93160a1fe59c565867777107e12f805802cbd1d7
Samantha Guergenenov <samguergen@gmail.com>
2016-05-22 22:26:55 -0400

    more comms removed and func polished

--- app/scripts/app-ctrl.js
+++ app/scripts/app-ctrl.js
@@ -7,49 +7,30 @@ angular.module('myApp')
 
   $scope.absentStudents = [];
 
-    // $scope.loadStudentJson = function() {
-    //     var httpRequest = $http({
-    //         method: 'GET',
-    //         url: 'https://script.googleusercontent.com/macros/echo?user_content_key=QKr2C80cYaXhiTmD1bc-KMeLbGgc6cjD4-_k2Vn0w_pJ5sg0Z9znaCYzrm6ue-5bEZu8Pfbsw5Kk-ikqJBkuh2BDytf3hoPXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFL1bTUvgtX-I3dP-rkaVkXwq9c0lP_yAdwLqrO1fBG9ViVZIXLuuM38ZxVVktS6pGwihnkohuAm&lib=Mw-ve0R7zJP9N1rf46jTbuILi7CGwAUIU',
-    //     }).success(function(data, status) {
-    //         $scope.students = data;
-    //     });
-    //     console.log('students are ', $scope.students);
-    // };
-
   $scope.loadStudentInfo = function() {
+    console.log('inside loadStudentInfo func');
     var httpRequest = $http({
         method : "GET",
         url : "https://script.googleusercontent.com/macros/echo?user_content_key=QKr2C80cYaXhiTmD1bc-KMeLbGgc6cjD4-_k2Vn0w_pJ5sg0Z9znaCYzrm6ue-5bEZu8Pfbsw5Kk-ikqJBkuh2BDytf3hoPXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFL1bTUvgtX-I3dP-rkaVkXwq9c0lP_yAdwLqrO1fBG9ViVZIXLuuM38ZxVVktS6pGwihnkohuAm&lib=Mw-ve0R7zJP9N1rf46jTbuILi7CGwAUIU"
     }).then(function mySuccess(response) {
         $scope.students = response.data;
-        // $scope.prettyStudents = JSON.stringify($scope.students);
     }, function myError(response) {
         $scope.error = response.statusText;
     });
   };
 
-  // $scope.absenceCheck = function() {
-  //   for (var student in $scope.students) {
-  //     if (student.attendance8thGrade {
-   //   }
-  //   }
-  // }
-
-  // $scope.attendance8thGrade = function() {
-  //   for (var student in $scope.students) {
-  //     if (student.attendance8thGrade {
-  //    }
-  //   }
-  // }
-
   $scope.checkAttendance = function(attendanceType) {
-    var absenceWindow = attendanceType;
     console.log('check attendance func initiated');
+    console.log('attendance type is ', attendanceType);
+
+    $scope.byAbs = true;
+    var absenceWindow = attendanceType;
+
     for (var student in $scope.students) {
       if ((student[absenceWindow] == 0) || (student[absenceWindow] == "")) {
          $scope.absentStudents << student;
      }
+     return $scope.absentStudents;
     }
   }
 
